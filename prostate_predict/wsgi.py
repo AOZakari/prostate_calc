2b9584f45777d0da5ad115bc9606190e49676f04
Zakari AIT OUAZZOU <aitouazzou.zakari@gmail.com>
2020-06-02 02:46:34 +0200

    Update wsgi.py

--- prostate_predict/wsgi.py
+++ prostate_predict/wsgi.py
@@ -14,7 +14,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'prostate_predict.settings')
 
 application = get_wsgi_application()
-
-from whitenoise.django import DjangoWhiteNoise
-
-application = DjangoWhiteNoise(application)
